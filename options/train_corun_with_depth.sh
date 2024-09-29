bb67d1c2981a818011c62851289a5cdbfc944230
chengyufang <cnyvfang@outlook.com>
2024-09-29 08:50:22 +0800

    Update the repo.

--- options/train_corun_with_depth.sh
+++ options/train_corun_with_depth.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-CUDA_VISIBLE_DEVICES=4  python3 -m torch.distributed.launch --nproc_per_node=1 --use_env  --master_port=4396 corun_colabator/train.py -opt options/train_corun_with_depth.yml --launcher pytorch
+CUDA_VISIBLE_DEVICES=4,5,6,7  python3 -m torch.distributed.launch --nproc_per_node=4 --use_env  --master_port=4396 corun_colabator/train.py -opt options/train_corun_with_depth.yml --launcher pytorch
