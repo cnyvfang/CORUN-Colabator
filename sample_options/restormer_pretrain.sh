f75cb5f07197dd80bab8ae3940e755e90a406496
chengyufang <cnyvfang@outlook.com>
2025-02-25 16:01:53 +0800

    Update

--- sample_options/restormer_pretrain.sh
+++ sample_options/restormer_pretrain.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-HF_HUB_OFFLINE=True CUDA_VISIBLE_DEVICES=4,5,6,7  python3 -m torch.distributed.launch --nproc_per_node=4 --use_env  --master_port=4396 corun_colabator/train.py -opt sample_options/restormer_sample.yml --launcher pytorch
+HF_HUB_OFFLINE=True CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --use_env  --master_port=4396 corun_colabator/train.py -opt sample_options/restormer_sample.yml --launcher pytorch
